6eda6d7d9ca9d2792c8e001395970a1c87ebd444
homow <homowsh897@gmail.com>
2025-08-23 20:34:13 +0330

    update and responsive NavBar in Header and Create .gitattributes file

--- src/jsx/component/Header/Header.jsx
+++ src/jsx/component/Header/Header.jsx
@@ -4,8 +4,10 @@ import NavBar from '@/jsx/component/Header/NavBar.jsx'
 
 function Header() {
     return (
-        <header className="h-screen bg-[url('/static/img/hero-bg.webp')] bg-cover bg-fixed">
-            <NavBar/>
+        <header className="h-screen pt-4 text-white bg-[url('/static/img/hero-bg.webp')] bg-cover bg-fixed">
+            <div className="container">
+                <NavBar/>
+            </div>
         </header>
     )
 }
--- src/jsx/component/Header/NavBar.jsx
+++ src/jsx/component/Header/NavBar.jsx
@@ -1,21 +1,75 @@
 import {StrictMode} from 'react'
 
-export default function NavBar() {
-    return (
-        <StrictMode>
-            <nav className={"flex items-center justify-between"}>
-                <div><a href="https://homow.ir"><img className={"w-full"} src="/static/logo/logo.webp" alt="logo"/></a></div>
-                <ul className={'flex items-center justify-between'}>
-                    <li><a href="/">خانه</a></li>
-                    <li><a href="">درباره ما</a></li>
-                    <li><a href="">خدمات</a></li>
-                    <li><a href="">نمونه کارها</a></li>
-                    <li><a href="">تیم</a></li>
-                    <li><a href="">لیست کشویی</a></li>
-                    <li><a href="">تماس با ما</a></li>
-                </ul>
-                <div><a href="https://homow.ir">شروع</a></div>
-            </nav>
-        </StrictMode>
-    )
+const navMenuHandler = elem => {
+    const flagMenu = elem.currentTarget.dataset.navMenu;
+    const openIcon = elem.currentTarget.querySelector("[data-icon-menu=open]");
+    const closeIcon = elem.currentTarget.querySelector("[data-icon-menu=close]");
+    const menu = document.getElementById("menu")
+    const overlay = document.getElementById("overlay");
+
+    elem.currentTarget.dataset.navMenu = flagMenu === "close" ? "open" : "close";
+
+    openIcon.classList.toggle("hidden", elem.currentTarget.dataset.navMenu === "open");
+    closeIcon.classList.toggle("hidden", elem.currentTarget.dataset.navMenu === "close");
+
+    if (elem.currentTarget.dataset.navMenu === "open") {
+        menu.style.right = "0"
+        overlay.classList.remove("hidden");
+    } else {
+        menu.style.right = "-180px"
+        overlay.classList.add("hidden");
+    }
 }
+
+function NavBar() {
+    return (<StrictMode>
+        <nav className={"flex items-center justify-between"}>
+            <div id="overlay" className="fixed inset-0 -bottom-2000 bg-black/60 z-10 hidden"></div>
+            <div className={"max-w-30"}><a href="https://homow.ir"><img className={"w-full"} src="/static/logo/logo.webp" alt="logo"/></a></div>
+            <ul className={'z-20 max-md:fixed top-0 -right-45 bottom-0 transition-all duration-150 flex gap-1 max-md:flex-col md:flex-row md:grow md:justify-center md:gap-5 lg:gap-12 font-dana-md max-md:text-black max-md:bg-white w-40'} id={"menu"}>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="/">خانه</a>
+                </li>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="">درباره ما</a>
+                </li>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="">خدمات</a>
+                </li>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="">نمونه کارها</a>
+                </li>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="">تیم</a>
+                </li>
+                <li>
+                    <a className={'block max-md:py-4 max-md:pr-4 max-md:hover:bg-amber-400 md:hover:text-amber-400 transition-all duration-300'} href="">تماس با ما</a>
+                </li>
+            </ul>
+            <div className={"cursor-pointer"}>
+                    <span onClick={navMenuHandler.bind(this)} data-nav-menu="close" className={"z-30 md:hidden"}>
+                        <svg data-icon-menu='open' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 z-30">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                        </svg>
+                        <svg data-icon-menu='close' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 hidden z-30">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                        </svg>
+                    </span>
+
+                <a className={'hidden md:block py-2 px-4 border border-amber-400 rounded-sm transition-all duration-250 hover:bg-amber-400 hover:text-black/90'} href="https://homow.ir">شروع</a>
+            </div>
+        </nav>
+    </StrictMode>)
+}
+
+window.onclick = e => {
+    const path = e.composedPath();
+    const btnNavMenu = document.querySelector("[data-nav-menu]");
+    const menu = document.getElementById("menu");
+
+    if (btnNavMenu && btnNavMenu.dataset.navMenu === "open" && !path.includes(menu) && !path.includes(btnNavMenu)) {
+        navMenuHandler({currentTarget: btnNavMenu});
+    }
+};
+
+export default NavBar

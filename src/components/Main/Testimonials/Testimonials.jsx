cc83c710e8ef8511582a5b5520b3f3af9f43b74a
homow <homowsh897@gmail.com>
2025-08-30 02:55:45 +0330

    FINISH

--- src/components/Main/Testimonials/Testimonials.jsx
+++ src/components/Main/Testimonials/Testimonials.jsx
@@ -1,8 +1,10 @@
+import {BASE_PATH} from "@/basePath.js";
+
 function Testimonials({img = "", title = "", subtitle = "", text = ""}) {
     return (
         <div className={"shrink-0 w-full px-4 py-2 space-y-4 text-white text-center"}>
             <div className={"overflow-hidden rounded-full border-4 border-white/20 mx-auto max-w-16"}>
-                <img loading={"lazy"} src={img} alt="img people" className={"w-full"}/>
+                <img loading={"lazy"} src={BASE_PATH + img} alt="img people" className={"w-full"}/>
             </div>
             <h4 className={"mt-2 text-2xl"}>{title}</h4>
             <h5 className={"text-gray-300"}>{subtitle}</h5>
